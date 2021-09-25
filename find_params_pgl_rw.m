689574bb62ca82632af9abaee0ee8cbe272a3b6e
Samuel Rey <samuel.rey.escudero@gmail.com>
2021-09-25 17:37:02 -0500

    adding recovered graphs and fixing scale of A

--- find_params_pgl_rw.m
+++ find_params_pgl_rw.m
@@ -9,18 +9,18 @@ N = 20;
 O = 19;
 p = 0.2;
 pert_links = 3;
-L = 3;
+F = 3;
 M = 1e4;
 sampled = true;
 hid_nodes = 'min';
 max_iters = 10;
 th = 0.3;
 
+deltas = [1e-2 1e-3 1e-4];
 gammas = [10 25 50 75 100];
 betas = [25 50 100];
 etas = [25 50 100];
 mus = [500 1000 2000];
-deltas = [1e-2 1e-3 1e-4];
 
 err = zeros(length(mus),length(etas),length(betas),length(gammas),...
     length(deltas),n_graphs);
@@ -36,10 +36,10 @@ parfor g=1:n_graphs
     % Create covariances
     Cs = zeros(N,N,K);
     for k=1:K
-        h = rand(L,1)*2-1;
+        h = rand(F,1)*2-1;
         H = zeros(N);
-        for l=1:L
-            H = H + h(l)*As(:,:,k)^(l-1);
+        for f=1:F
+            H = H + h(f)*As(:,:,k)^(f-1);
         end
         
         if sampled
@@ -67,8 +67,8 @@ parfor g=1:n_graphs
             regs.gamma = gammas(j);
             for k=1:length(betas)
                 regs.beta = betas(k);
-                for l=1:length(etas)
-                    regs.eta = etas(l);
+                for f=1:length(etas)
+                    regs.eta = etas(f);
                     for m=1:length(mus)
                         regs.mu = mus(m);
                         [Ao_hat,~] = estA_pgl_colsp_rw2(Co,N-O,regs,max_iters);
@@ -76,16 +76,16 @@ parfor g=1:n_graphs
                         diff_Ao = Ao-Ao_hat;
                         for n=1:K
                             norm_Ao = norm(Ao(:,:,n),'fro')^2;
-                            err_g(n,m,l,k,j,o) = norm(diff_Ao(:,:,n),'fro')^2/norm_Ao;
+                            err_g(n,m,f,k,j,o) = norm(diff_Ao(:,:,n),'fro')^2/norm_Ao;
                             
                             Ao_th = Ao(:,:,k);
                             Ao_th(Ao_th >= th) = 1;
                             Ao_th(Ao_th < th) = 0;
                             
-                            Ao_hat_th = Ao(:,:,k);
+                            Ao_hat_th = Ao_hat(:,:,k);
                             Ao_hat_th(Ao_hat_th >= th) = 1;
                             Ao_hat_th(Ao_hat_th < th) = 0;
-                            [~,~,fsc_g(n,m,l,k,j,o),~,~] = ...
+                            [~,~,fsc_g(n,m,f,k,j,o),~,~] = ...
                                 graph_learning_perf_eval(Ao_th,Ao_hat_th);
                         end
                     end
@@ -99,12 +99,12 @@ end
 t = toc;
 disp(['--- ' num2str(t/3600) ' hours'])
 %%
-mean_err = mean(err,7);
+mean_err = mean(err,6);
 min(mean_err(:))
-median_err = median(err,7);
+median_err = median(err,6);
 min(median_err(:))
 
-mean_fsc = mean(fsc,7);
+mean_fsc = mean(fsc,6);
 max(mean_fsc(:))
-median_err = median(fsc,7);
+median_err = median(fsc,6);
 max(median_fsc(:))
--- number_samples_infl.m
+++ number_samples_infl.m
@@ -3,7 +3,7 @@ rng(10)
 addpath(genpath('utils'));
 addpath(genpath('opt'));
 
-n_graphs = 5;
+n_graphs = 20;
 K = 3;
 N = 20;
 O = 19;
@@ -16,21 +16,22 @@ th = 0.3;
 
 leg = {'G1, joint','G2, joint','G3, joint','G1, sep','G2, sep','G3, sep'};
 
-
 max_iters = 10;
 regs = struct();
 regs.alpha   = 1;       % Sparsity of S
-regs.gamma   = 50;      % Group Lasso
-regs.beta    = 50;      % Similarity of S
+regs.gamma   = 75;      % Group Lasso
+regs.beta    = 25;      % Similarity of S
 regs.eta     = 50;      % Similarity of P
-regs.mu      = 1000;    % Commutative penalty
-regs.delta1  = 1e-3;    % Small number for reweighted
+regs.mu      = 250;    % Commutative penalty
+regs.delta1  = 1e-2;    % Small number for reweighted
+
+max_M = Ms(end);
 
 tic
 Aos_joint = zeros(O,O,K,length(Ms),n_graphs);
 Aos_sep = zeros(O,O,K,length(Ms),n_graphs);
 Aos = zeros(O,O,K,n_graphs);
-for g=1:n_graphs
+parfor g=1:n_graphs
     disp(['G: ' num2str(g)])
     
     % Create graphs and get hidden nodes
@@ -38,37 +39,41 @@ for g=1:n_graphs
     As = gen_similar_graphs(A,K,pert_links);
     [n_o, n_h] = select_hidden_nodes(hid_nodes, O, As(:,:,1));
     
+    Ao = As(n_o,n_o,:);
+    Aoh = As(n_o,n_h,:);
         
+    % Generate signals X
+    X = zeros(N,max_M,K);
+    for k=1:K
+        h = rand(F,1)*2-1;
+        H = zeros(N);
+        for f=1:F
+            H = H + h(f)*As(:,:,k)^(f-1);
+        end
+        X(:,:,k) = H*randn(N,max_M);
+    end
+    
     Aos_joint_g = zeros(O,O,K,length(Ms));
     Aos_sep_g = zeros(O,O,K,length(Ms));
-    Aos_g = zeros(O,O,K,length(Ms));
     for i=1:length(Ms)
         M = Ms(i);
         disp(['   M: ' num2str(M)])
         
-        % Generate covariance matrix
+        % Compute covariance
         Cs = zeros(N,N,K);
         for k=1:K
-            h = rand(F,1)*2-1;
-            H = zeros(N);
-            for f=1:F
-                H = H + h(f)*As(:,:,k)^(f-1);
-            end
-            X = H*randn(N,M);
-            Cs(:,:,k) = X*X'/M;
+            Cs(:,:,k) = X(:,1:M,k)*X(:,1:M,k)'/M;
         end
-        
-        Ao = As(n_o,n_o,:);
-        Aoh = As(n_o,n_h,:);
         Co = Cs(n_o,n_o,:);
-        Coh = Cs(n_o,n_h,:);
+%         Coh = Cs(n_o,n_h,:);
         
         % Joint inference
-        Aos_joint_g(:,:,:,i) = estA_pgl_colsp_rw2(Co,N-O,regs,max_iters);
-        
+        Ao_hat_j = estA_pgl_colsp_rw2(Co,N-O,regs,max_iters);
+        Aos_joint_g(:,:,:,i) = Ao_hat_j./max(max(Ao_hat_j));
         % Separate inference
         for k=1:K
-            Aos_sep_g(:,:,k,i) = estA_pgl_colsp_rw2(Co(:,:,k),N-O,regs,max_iters);
+            Ao_hat_s = estA_pgl_colsp_rw2(Co(:,:,k),N-O,regs,max_iters);
+            Aos_sep_g(:,:,k,i) = Ao_hat_s./max(max(Ao_hat_s));
         end
     end
     Aos(:,:,:,g) = Ao;
@@ -81,10 +86,10 @@ disp(['----- ' num2str(t/3600) ' hours -----'])
 %% Plot mean/median error
 err_joint = zeros(K,length(Ms),n_graphs);
 err_sep = zeros(K,length(Ms),n_graphs);
-for k=1:K
-    for i=1:length(Ms)
-        for g=1:n_graphs
-            norm_A = norm(Aos(:,:,k,g),'fro')^2;
+for g=1:n_graphs
+    for k=1:K
+        norm_A = norm(Aos(:,:,k,g),'fro')^2;
+        for i=1:length(Ms)
             err_joint(k,i,g) = ...
                 norm(Aos(:,:,k,g)-Aos_joint(:,:,k,i,g),'fro')^2/norm_A;
             err_sep(k,i,g) = ...
