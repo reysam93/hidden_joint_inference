26964ad4807fbf4c239e78aee8bb89c4c6e757c3
Andrei <andreibuciulea@gmail.com>
2021-09-27 10:47:07 +0200

    Update test file

--- test_estA_lowrank.m
+++ test_estA_lowrank.m
@@ -4,7 +4,7 @@ addpath(genpath('utils'));
 addpath(genpath('opt'));
 
 close all
-K = 3;
+Ks = [3,6];
 N = 20;
 O = 15;
 HH = N-O;
@@ -22,58 +22,62 @@ prms.epsilon = 1e-6;
 % Regs for reweighted
 prms.delta1 = 1e-3;
 prms.delta2 = 1e-3;
-prms.max_iters = 5;
+prms.max_iters = 15;
 
 hid_nodes = 'min';
-nG = 12;
+nG = 64;
 %models = {'baseline','lowrank','lowrank rw','grouplasso','grouplasso rw'};
-models = {'lowrank','grouplasso'};
+models = {'lowrank rw','grouplasso rw'};
 
 % Create graphs
-As = zeros(N,N,K);
-err_joint = zeros(nG,numel(models),HH,K);
-err_sep = zeros(nG,numel(models),HH,K);
+As = zeros(N,N,Ks(end));
+err_joint = zeros(nG,numel(Ks),numel(models),HH,Ks(end));
+err_sep = zeros(nG,numel(Ks),numel(models),HH,Ks(end));
 tic
-A_T = cell(nG,numel(models),HH);
-parfor g = 1:nG
+A_T = cell(nG,numel(Ks),numel(models),HH);
+for g = 1:nG
     A = generate_connected_ER(N,p);
     A_org = A;
-    err_joint_g = zeros(numel(models),HH,K);
-    err_sep_g = zeros(numel(models),HH,K);
-    A_T_g = cell(numel(models),HH);
+    err_joint_g = zeros(numel(Ks),numel(models),HH,Ks(end));
+    err_sep_g = zeros(numel(Ks),numel(models),HH,Ks(end));
+    A_T_g = cell(numel(Ks),numel(models),HH);
     for m = 1:numel(models)
         model = models{m};
         regs = get_regs(model,prms);
-        As = gen_similar_graphs(A,K,pert_links);
+        As = gen_similar_graphs(A,Ks(end),pert_links);
         Cs = create_cov(As,L,M,sampled);
-        A_H = cell(1,5);
+        A_H = cell(numel(Ks),HH);
         for hd = 1:HH
         
             % Same observed/hidden nodes for all graphs
             [n_o, n_h] = select_hidden_nodes(hid_nodes, N-hd, As(:,:,1));
-            Ao = As(n_o,n_o,:);
-            Co = Cs(n_o,n_o,:);
-            
-            %Estimate graph structure
-            [Ao_hat,Ao_hat_sep] = estimate_A(model,Co,regs);
-            
-            Ao_hat = Ao_hat./max(max(Ao_hat));
-            Ao_hat_sep = Ao_hat_sep./max(max(Ao_hat_sep));
-            A_aux = zeros(3,N-hd,N-hd,3);
-            A_aux(1,:,:,:) = Ao;A_aux(2,:,:,:) = Ao_hat; A_aux(3,:,:,:) = Ao_hat_sep;  
-            A_H{hd} = A_aux;
-            
-            for k=1:K
-                norm_Ao = norm(Ao(:,:,k),'fro')^2;
-                err_joint_g(m,hd,k) = norm(Ao(:,:,k)-Ao_hat(:,:,k),'fro')^2/norm_Ao;
-                err_sep_g(m,hd,k) = norm(Ao(:,:,k)-Ao_hat_sep(:,:,k),'fro')^2/norm_Ao;
+            for nk = 1:numel(Ks)
+                K = Ks(nk);
+                Ao = As(n_o,n_o,1:K);
+                Co = Cs(n_o,n_o,1:K);
+
+                %Estimate graph structure
+                [Ao_hat,Ao_hat_sep] = estimate_A(model,Co,regs);
+
+                Ao_hat = Ao_hat./max(max(Ao_hat));
+                Ao_hat_sep = Ao_hat_sep./max(max(Ao_hat_sep));
+                A_aux = zeros(3,N-hd,N-hd,K);
+                A_aux(1,:,:,:) = Ao;A_aux(2,:,:,:) = Ao_hat; A_aux(3,:,:,:) = Ao_hat_sep;  
+                A_H{nk,hd} = A_aux;
+
+                for k=1:K
+                    norm_Ao = norm(Ao(:,:,k),'fro')^2;
+                    %guardar los errores de K=3 y K = 6
+                    err_joint_g(nk,m,hd,k) = norm(Ao(:,:,k)-Ao_hat(:,:,k),'fro')^2/norm_Ao;
+                    err_sep_g(nk,m,hd,k) = norm(Ao(:,:,k)-Ao_hat_sep(:,:,k),'fro')^2/norm_Ao;
+                end
             end
         end
-        A_T_g(m,:) = A_H;
+        A_T_g(m,:,:) = A_H;
     end
-    A_T(g,:,:) = A_T_g;
-    err_joint(g,:,:,:) = err_joint_g;
-    err_sep(g,:,:,:) = err_sep_g;
+    A_T(g,:,:,:) = A_T_g;
+    err_joint(g,:,:,:,:) = err_joint_g;
+    err_sep(g,:,:,:,:) = err_sep_g;
     
 end
 toc
