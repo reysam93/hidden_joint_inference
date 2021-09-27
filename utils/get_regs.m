eda98502c3ed455e6d8dfac140569a50374dc724
Andrei <andreibuciulea@gmail.com>
2021-09-27 10:46:00 +0200

    Update regularizers for PNN rw and PGL rw

--- utils/get_regs.m
+++ utils/get_regs.m
@@ -9,35 +9,35 @@ function regs = get_regs(model,prms)
         regs.alpha = 1e-2;
         regs.gamma = 1; 
         regs.beta = 1e-1;  
-        regs.eta = 1;
+        regs.eta = 1; 
     elseif strcmp(model,'lowrank rw')
-%         regs.alpha = 1e-2;
-%         regs.gamma = 1e1; 
-%         regs.beta = 1e-1;  
-%         regs.eta = 1e-1;
-%         regs.mu = 1e2; 
-
-        regs.alpha = 1;
+        regs.alpha = 1e-1;
         regs.gamma = 1e2; 
-        regs.beta = 1e1;  
+        regs.beta = 1;  
         regs.eta = 1;
         regs.mu = 1e2; 
     elseif strcmp(model,'grouplasso')
         regs.alpha = 1e-2;
-        regs.gamma = 21.54;
+        regs.gamma = 1;
         regs.mu = 1;
-        regs.beta = 2.154;  
+        regs.beta = 1;  
         regs.eta = 1;
     elseif strcmp(model,'grouplasso rw')
-        regs.alpha = 1;
+        regs.alpha = 1e-1;
         regs.gamma = 1e2; 
-        regs.beta = 1e1;  
-        regs.eta = 1;
-        regs.mu = 1e2; 
+        regs.beta = 1;  
+        regs.eta = 1e-2;
+        regs.mu = 1e3; 
     elseif strcmp(model,'baseline')
         regs.alpha = 1e-2; 
         regs.beta = 1e2;
         regs.epsilon = 1e-1;
+    elseif strcmp(model,'baseline rw')
+        regs.alpha = 1e-2; 
+        regs.beta = 1e-1;
+        regs.mu = 1;
+    else 
+        disp('Unknown algorithm')
     end
 end
 
