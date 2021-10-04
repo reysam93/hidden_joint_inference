8ebea0f034a4ddbd629c4ee8c646fdd4c17fc265
Andrei <andreibuciulea@gmail.com>
2021-10-04 21:41:27 +0200

    Updating create cov

--- utils/create_cov.m
+++ utils/create_cov.m
@@ -1,39 +1,22 @@
-function [Cs] = create_cov(As,prms)
+function [Cs] = create_cov(As,L,M,sampled)
     % Create covariances
-    L = prms.L;
-    M = prms.M;
-    sampled = prms.sampled;
-    Ctype = prms.Ctype;
     N = size(As,1);
     K = size(As,3);
     Cs = zeros(size(As));
     for k=1:K
-        if strcmp(Ctype,'Cpoly')
-            %h = rand(L,1)*2-1;
-            h = randn(L,1);
-            h = h/norm(h,1);
-            H = zeros(N);
-            for l=1:L
-                H = H + h(l)*As(:,:,k)^(l-1);
-            end
+        %h = rand(L,1)*2-1;
+        h = randn(L,1);
+        h = h/norm(h,1);
+        H = zeros(N);
+        for l=1:L
+            H = H + h(l)*As(:,:,k)^(l-1);
+        end
 
-            if sampled
-                X = H*randn(N,M);
-                Cs(:,:,k) = X*X'/M;
-            else
-                Cs(:,:,k) = H^2;
-            end
-        elseif strcmp(Ctype,'Cmrf')
-            eigvals = eig(As(:,:,k));
-            C_inv = (0.01-min(eigvals))*eye(N,N) + (0.9+0.1*rand(1,1))*As(:,:,k);
-            Cs(:,:,k) = inv(C_inv);
-            if sampled
-                W = randn(N,M);
-                X(:,:,k) = sqrtm(Cs(:,:,k))*W;
-                Cs(:,:,k) = X(:,1:M,k)*X(:,1:M,k)'/M;
-            end
+        if sampled
+            X = H*randn(N,M);
+            Cs(:,:,k) = X*X'/M;
         else
-            disp('Unknown covariance model')
+            Cs(:,:,k) = H^2;
         end
     end
 end
