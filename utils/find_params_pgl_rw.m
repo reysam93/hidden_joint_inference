320d0df728d9a75ca424d7cf84586eb00821f817
Samuel Rey <samuel.rey.escudero@gmail.com>
2023-01-04 11:47:31 +0100

    showing best hyperparams

--- utils/find_params_pgl_rw.m
+++ utils/find_params_pgl_rw.m
@@ -15,9 +15,8 @@ sampled = true;
 hid_nodes = 'min';
 max_iters = 10;
 th = 0.3;
-Cmrf = false;
+C_type = 'st';
 verb_freq = 10;
-
 
 deltas = [1e-2 1e-3 1e-4];
 gammas = [10 25 50 75 100];
@@ -27,49 +26,22 @@ mus = [500 1000 2000];
 
 err = zeros(length(mus),length(etas),length(betas),length(gammas),...
     length(deltas),n_graphs);
-fsc = zeros(length(mus),length(etas),length(betas),length(gammas),...
-    length(deltas),n_graphs);
 tic
 parfor g=1:n_graphs
+    % Generate data
     A = generate_connected_ER(N,p);
     As = gen_similar_graphs(A,K,pert_links);
     [n_o, n_h] = select_hidden_nodes(hid_nodes, O, As(:,:,1));
-    
-    % Create covariances
-    Cs = create_cov(As,F,M,sampled); 
-%     Cs = zeros(N,N,K);
-%     for k=1:K
-%         if Cmrf
-%             eigvals = eig(As(:,:,k));
-%             C_inv = (0.01-min(eigvals))*eye(N,N) + (0.9+0.1*rand(1,1))*As(:,:,k);
-%             C_true = inv(C_inv);
-%         else
-%             h = rand(F,1)*2-1;
-%             H = zeros(N);
-%             for f=1:F
-%                 H = H + h(f)*As(:,:,k)^(f-1);
-%             end
-%             C_true = H^2;
-%         end
-%         
-%         if sampled
-%             X = sqrtm(C_true)*randn(N,M);
-%             Cs(:,:,k) = X*X'/M;
-%         else
-%             Cs(:,:,k) = C_true;
-%         end
-%     end
-    
+    Cs = create_cov(As,F,M,sampled, C_type); 
     Ao = As(n_o,n_o,:);
-    Aoh = As(n_o,n_h,:);
     Co = Cs(n_o,n_o,:);
-    Coh = Cs(n_o,n_h,:);
     
     regs = struct();
     regs.alpha = 1;
-    err_g = zeros(K,length(mus),length(etas),length(betas),...
+
+    err_g = zeros(length(mus),length(etas),length(betas),...
         length(gammas),length(deltas));
-    fsc_g = zeros(K,length(mus),length(etas),length(betas),...
+    fsc_g = zeros(length(mus),length(etas),length(betas),...
         length(gammas),length(deltas));
     for o=1:length(deltas)
         regs.delta1 = deltas(o);
@@ -87,20 +59,11 @@ parfor g=1:n_graphs
                         diff_Ao = Ao-Ao_hat;
                         for n=1:K
                             norm_Ao = norm(Ao(:,:,n),'fro');
-                            err_g(n,m,f,k,j,o) = (norm(diff_Ao(:,:,n),'fro')/norm_Ao)^2;
-                            
-                            Ao_th = Ao(:,:,n);
-                            Ao_th(Ao_th >= th) = 1;
-                            Ao_th(Ao_th < th) = 0;
-                            
-                            Ao_hat_th = Ao_hat(:,:,n);
-                            Ao_hat_th(Ao_hat_th >= th) = 1;
-                            Ao_hat_th(Ao_hat_th < th) = 0;
-                            [~,~,fsc_g(n,m,f,k,j,o),~,~] = ...
-                                graph_learning_perf_eval(Ao_th,Ao_hat_th);
+                            err_g(n,m,f,k,j,o) = err_g(n,m,f,k,j,o) + ...
+                                (norm(diff_Ao(:,:,n),'fro')/norm_Ao)^2/K;
                         end
 
-                        if mod(n_graphs,verb_freq) == 1
+                        if mod(G ,verb_freq) == 1
                             disp(['Graph: ' num2str(g) ' delta: ' num2str(regs.delta1) ...
                                 ' gamma: ' num2str(regs.gamma) ' beta: ' ...
                                 num2str(regs.beta) ' eta: ' num2str(regs.eta) ...
@@ -112,20 +75,19 @@ parfor g=1:n_graphs
             end
         end
     end
-    err(:,:,:,:,:,g) = mean(err_g,1);
-    fsc(:,:,:,:,:,g) = mean(fsc_g,1);
+    err(:,:,:,:,:,g) = err_g;
 end
 t = toc;
 disp(['--- ' num2str(t/3600) ' hours'])
 %% Display results
 mean_err = mean(err,6);
 median_err = median(err,6);
-mean_fsc = mean(fsc,6);
-median_fsc = median(fsc,6);
-rec_graphs = sum(fsc==1,6)/n_graphs;
+[min_err, lin_idx] = min(mean_err(:));
+[m,f,k,j,o] = ind2sub(size(mean_err),lin_idx);
+
+disp(['Delta: ' num2str(deltas(o)) ' Gamma: ' num2str(gammas(j)) ' Beta: ' ...
+    num2str(betas(k)) ' Eta: ' num2str(etas(f)) ' Mu: ' num2str(mus(m)) ...
+    ' Err: ' num2str(mean_err(m,f,k,j,o))])
 
 disp(['Min mean err: ' num2str(min(mean_err(:)))])
 disp(['Min median err: ' num2str(min(median_err(:)))])
-disp(['Max mean fsc: ' num2str(max(mean_fsc(:)))])
-disp(['Max median err: ' num2str(max(median_fsc(:)))])
-disp(['Max rec: ' num2str(max(rec_graphs(:)))])
